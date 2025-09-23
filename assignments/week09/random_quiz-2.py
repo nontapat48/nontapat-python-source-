1dd6721d38435367ffae3011d0f1b4e6b4a5ce57
nontapat nakpoo <nontapat.na@ku.th>
2025-09-23 11:47:48 +0700

    .

--- assignments/week09/random_quiz-2.py
+++ assignments/week09/random_quiz-2.py
@@ -58,8 +58,34 @@ def get_divisibility_hint(number):
 
 def get_range_hint(number, current_min=1, current_max=100):
     # Return narrowed range around the number
-    pass
+    return f"HINT: The narrow range amount the number is {range(number - 12, number + 12)}"
 
 def get_thefirst_digit_hint(number):
     # Retun the first digit of the number
-    pass
+    first_digit = number //10
+    return f"HINT: The first digit is {first_digit}"
+
+random_number = random.randint(1, 100)
+attempt = 1
+
+while True:
+    guess_number = int(input(f"Attempt {attempt} - Enter your guess: "))
+
+    if random_number == guess_number:
+        print(f"Congratulations! You won in {attempt} attempts!")
+        break
+    elif random_number < guess_number:
+        print("Too high! Try again.")
+    else:
+        print("Too low! Try again.")
+
+    if attempt == 3:
+        get_parity_hint(random_number)
+    elif attempt == 5:
+        print(get_divisibility_hint(random_number))
+    elif attempt == 7:
+        print(get_range_hint(random_number))
+    elif attempt == 10:
+        print(get_thefirst_digit_hint(random_number))
+
+    attempt = attempt + 1
