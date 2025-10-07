106965cc8db5525166c9c3e2acb08718974e2471
nontapat nakpoo <nontapat.na@ku.th>
2025-10-07 11:32:22 +0700

    .

--- assignments/week11/grade.py
+++ assignments/week11/grade.py
@@ -20,3 +20,41 @@ A global variable passing_grade = 50
 - Uses the global passing_grade to determine pass/fail
 - Prints each student's name, average, and status (PASS/FAIL)
 """
+
+passing_grade = 50
+
+def input_student(num_students=0):
+    students = [
+        {
+            'name': 'nontapat',
+            'scores': [35, 54, 60]
+        },
+        {
+            'name': 'Mary',
+            'scores': [87, 75, 93]
+        }
+    ]
+    return students
+
+def calculate_averages(students):
+    for student in students:
+        sum_score = 0
+        for score in student ['scores']:
+            sum_score = sum_score + score
+        student['average'] = sum_score / 3
+    return students
+
+def display_results(students):
+    print("Student Details")
+    for student in students:
+        print(f"Name: {student['name']}")
+        print("Average Score: %.1f % {student['average']}")
+        if student['average'] > passing_grade:
+            print("Status: PASS")
+        else:
+            print("Status: FAIL")
+
+
+students = input_student()
+students = calculate_averages(students)
+display_results(students)
--- assignments/week11/temperature.py
+++ assignments/week11/temperature.py
@@ -22,4 +22,28 @@ Highest: 28 C
 Lowest: 19 C
 
 """
-    
+
+
+def get_temperatures():
+    x = [32, 30, 31, 30, 31, 34]
+    return x
+
+def analyze_temps(temp_list):
+    average_temp = 0
+    sum = 0
+    for temp in temp_list:
+        sum = sum + temp
+    average_temp = sum / 7
+    max_temp = max(temp_list)
+    min_temp = min(temp_list)
+    return (average_temp, max_temp, min_temp)
+
+def display_analysis(avg, high, low):
+    print("Temperature Analysis for the Week: ")
+    print("Average: %.1f C" % (avg))
+    print(f"Highest: {high} C")
+    print("Lowest: %d C" % (low))
+
+temp_list = get_temperatures()
+result = analyze_temps(temp_list)
+display_analysis(result[0], result[1], result[2])
